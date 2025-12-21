fe162fe120fc02e5bac425eec1c488ae0501ec9d
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-12-21 12:37:11 -0400

    add `qt5ct` and `kvantum-qt5`

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm librecad qt5-wayland
+pacman -Syu --noconfirm librecad qt5-wayland qt5ct kvantum-qt5
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
